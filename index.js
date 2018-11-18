f0ac91b079228ad3b4af856035282d597df59fe9
gleb-mihalkov <gmihalkov123@gmail.com>
2018-11-18 22:12:41 +0300

    Testing commander package

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
 'use strict';
+const program = require('commander');
+const info = require('./package.json');
+
+program
+  .version(info.version, '-v, --version')
+  .command('watch', 'starts the watcher which keeps the branches syncronized')
+  .command('init', 'runs the initialiation master')
+  .parse(process.argv);
